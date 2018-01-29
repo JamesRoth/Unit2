3610a4700e6cf7da5fef0d5ee524b22d1902515c
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-29 08:30:50 -0500

    Updated from Brython Server: 1/29/2018 8:30:49 AM

--- movie.py
+++ movie.py
@@ -1,1 +1,11 @@
+#James Roth
+#1/29/18
+#movie.py - more if statements
 
+age=input(int("What is your age?: "))
+if age>16:
+    print("You can watch R movies.")
+elif age>12:
+    print("You can watch PG-13 movies.")
+else:
+    print("You can watch PG movies.")
