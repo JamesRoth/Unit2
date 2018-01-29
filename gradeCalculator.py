12e589c81071f23eddc15cacc66730b7c49225ed
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-29 08:25:23 -0500

    Updated from Brython Server: 1/29/2018 8:25:22 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -7,3 +7,9 @@ if grade>89:
     print("You earned an A")
 elif grade>79:
     print("You earned a B")
+elif grade>69:
+    print("You earned a C")
+elif grade>59:
+    print("You earned a D")
+else:
+    print("You earned an NC")
