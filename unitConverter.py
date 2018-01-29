458cd26d3f246998cffaab1a40a7b943fdb4f818
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-29 08:42:34 -0500

    Updated from Brython Server: 1/29/2018 8:42:34 AM

--- unitConverter.py
+++ unitConverter.py
@@ -1,3 +1,6 @@
 #James Roth
 #1/29/18
 #unitConverter.py - converting units with if statements
+
+convert=int(input("What would you like to convert? Enter 1 for KM to Miles, 2 for KG to LBS, 3 for Liters to Gallons, and 4 for Celsius to Fahrenheight"))
+
