6821a41c2e7737d76bf04c4f55ef204639271ecd
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-29 08:23:08 -0500

    Updated from Brython Server: 1/29/2018 8:23:07 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,9 @@
+#James Roth
+#1/29/18
+#gradeCalculator.py - if statements to calc grades
 
+grade=int(input("Enter your grade: ")
+if grade>89:
+    print("You earned an A")
+elif grade>79:
+    print("You earned a B")
