4818a2edbbaa11f45969d25b1c714ecdaa2db9d1
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-29 08:11:20 -0500

    Updated from Brython Server: 1/29/2018 8:11:19 AM

--- ifDemo.py
+++ ifDemo.py
@@ -1,7 +1,9 @@
 #James Roth
 #1/29/18
-#ifDemo.py - inrot to if statements
+#ifDemo.py - inrot to if statements - if, elif, else
 num=float(input("Enter a number: "))
 
 if num>0:
     print(num, "is positive.")
+elif num<0:
+    print(num, "is negative.")
