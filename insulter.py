ccd2c05c798e7ba606c9724144db985bfbff7dfb
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-30 08:46:52 -0500

    Updated from Brython Server: 1/30/2018 8:46:52 AM

--- insulter.py
+++ insulter.py
@@ -10,6 +10,10 @@ if insult==1:
     print("You have the brains of a dead fly.")
 elif insult==2: 
     print("You're a waste of air.")
-eilf insult==3:
-    print("
+elif insult==3:
+    print("Calling you worthless is an understatement.")
+elif insult==4: 
+    print("You're the definition of dissapointment.")
+else:
+    print("Cats are smarter than you.")
 
