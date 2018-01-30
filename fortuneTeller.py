34be3ee4507a959f8d8318d142a9b49d83b570ef
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-30 08:33:34 -0500

    Updated from Brython Server: 1/30/2018 8:33:33 AM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -1,1 +1,25 @@
+#James Roth
+#1/30/18
+#fortuneTeller.py - different if statements
 
+color=input("Pick a color: red or green: ")
+num=int(input("Pick a number from 1-4: "))
+
+if color==red:
+    if num==1:
+        print("You will lose your pet fish.")
+    elif num==2:
+        print("You will break an arm.")
+    elif num==3:
+        print("You will win a car.")
+    elif num==4:
+        print("You will get struck by lightning.")
+elif color==blue:
+    if num==1:
+        print("You will get hit by a car.")
+    elif num==2:
+        print("You will win the lottery.")
+    elif num==3:
+        print("North Korea will invade the US.")
+    elif num==4:
+        print("")
