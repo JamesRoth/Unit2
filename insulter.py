dbf3ed2acd882d88be66ed42c5f4767f31ea27cd
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-30 08:45:02 -0500

    Updated from Brython Server: 1/30/2018 8:45:02 AM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,15 @@
+#James Roth
+#1/30/18
+#insulter.py - random ints and choosing statements
+
+from random import randint
+
+name=input("Enter your name: ")
+insult=int(randint(1,5))
+if insult==1:
+    print("You have the brains of a dead fly.")
+elif insult==2: 
+    print("You're a waste of air.")
+eilf insult==3:
+    print("
 
