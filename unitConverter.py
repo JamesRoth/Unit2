e5d556afe7408903ffd90507a82fdbf0965fc081
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-30 08:24:09 -0500

    Updated from Brython Server: 1/30/2018 8:24:08 AM

--- unitConverter.py
+++ unitConverter.py
@@ -6,7 +6,15 @@ convert=float(input("What would you like to convert? Enter 1 for KM to Miles, 2 
 if convert==1:
     km=float(input("Enter amount of KM: "))
     print(km, "kilometers is", round(km*0.621371, 1), "miles.")
-if convert==2:
+elif convert==2:
     kg=float(input("Enter a number of kilograms: ")
     print(kg, "kilograms is", kg*2.20462, "pounds.")
-if 
+elif convert==3:
+    liters=float(input("Enter a number of liters: ")
+    print(liters, "liters is", liters*0.264172, "gallons.")
+elif convert==4:
+    celsius=float(input("Enter a temp in celsius : ")
+    print(celsius, "celsius is", celsius*(9/5)+32, "degress fahrenheight.")
+else:
+    print("Error")
+
