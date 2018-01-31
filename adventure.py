79e8295e65f1da497777aa569b2fc3b96d35c788
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-31 08:38:05 -0500

    Updated from Brython Server: 1/31/2018 8:38:04 AM

--- adventure.py
+++ adventure.py
@@ -8,4 +8,11 @@ choice=int(input("Press 1 to follow the path, press 2 to climb the tall tree to 
 if choice==1:
     print("You follow the path for hours. It's hot, buggy, humid, and miserable, and the dense undergrowth eventually slows your progess to a mere crawl. ")
     choice=int(input("Do you keep folowing the path (1), or do you turn back and try to get back where you started? (2) "))
+    if choice==1:
+        
     
+    else:
+        
+
+elif choice==2:
+    print("It's a tough climb. The tree is covered in vines and slippery, decomposing leaves. Eventually, after much effort and a few close calls, you get to the top. Apparently you climbed the tallest tree in the jungle, and you're affored an unspoiled view of the surrounding jungle. However, you see a storm coming in and its inevitable rain will make the already dangerous climb down even worse. However, you've had no time to look around you  How long do you stay up in the tree?
