77d83a4073c0e3ee32321b7602a073fe4fef0287
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-31 08:29:13 -0500

    Updated from Brython Server: 1/31/2018 8:29:12 AM

--- adventure.py
+++ adventure.py
@@ -1,1 +1,7 @@
+#James Roth
+#1/31/18
+#adventure.py - nested conditional statements
+
+print("You wake up in the middle of a dense forest. You look around and see an old path behind you, a tall tree to your right, and a dark cave to your left.")
+choice=int(input("Press 1 to follow the path, press 2 to climb the tall tree to look around, and press 3 to explore the cave." ))
 
