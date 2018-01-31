3df7d2066d22301ff5bb845ed31e430178b57125
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-31 08:17:39 -0500

    Updated from Brython Server: 1/31/2018 8:17:38 AM

--- warmup3.py
+++ warmup3.py
@@ -10,5 +10,5 @@ if num/2==0:
         print(num, "is divisible by 2.")
 elif num/3==0:
     print(num, "is divisible by 3.")
-elif num/2==0 and num/3==0:
+else:
     print(num, "is not divisible by 2 or 3.")
