f24dc6796d8073778e4b3e5c743ff47033aaa7d0
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-31 08:33:01 -0500

    Updated from Brython Server: 1/31/2018 8:33:00 AM

--- adventure.py
+++ adventure.py
@@ -5,3 +5,7 @@
 print("You wake up in the middle of a dense forest. You look around and see an old path behind you, a tall tree to your right, and a dark cave to your left.")
 choice=int(input("Press 1 to follow the path, press 2 to climb the tall tree to look around, and press 3 to explore the cave." ))
 
+if choice==1:
+    print("You follow the path for hours. It's hot, buggy, humid, and miserable, and the dense undergrowth eventually slows your progess to a mere crawl. ")
+    choice=int(input("Do you keep folowing the path (1), or do you turn back and try to get back where you started? (2) "))
+    
