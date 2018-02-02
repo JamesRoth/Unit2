1889343ae09d79aa10e84a570657149d1c2046ac
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-02 08:37:07 -0500

    Updated from Brython Server: 2/2/2018 8:37:07 AM

--- coloredSquare.py
+++ coloredSquare.py
@@ -3,9 +3,26 @@
 #coloredSquare.py - graphics and RGB
 
 from ggame import *
-red=Color(0xff0000,1)
-line=LineStyle(3,red)
-rectangle=RectangleAsset(100,100,line,red)
+
+color=int(input("Red (1), yellow (2), green (3), or blue (4) ?"))
+if color==1:
+    red=Color(0xff0000,1)
+    line=LineStyle(3,red)
+    rectangle=RectangleAsset(100,100,line,red)
+elif color==2:
+    red=Color(0xff0000,1)
+    line=LineStyle(3,red)
+    rectangle=RectangleAsset(100,100,line,red)
+elif color==3:
+    red=Color(0xff0000,1)
+    line=LineStyle(3,red)
+    rectangle=RectangleAsset(100,100,line,red)
+else:
+    red=Color(0xff0000,1)
+    line=LineStyle(3,red)
+    rectangle=RectangleAsset(100,100,line,red)
+
+
 Sprite(rectangle)
 myApp=App()
 myApp.run()
