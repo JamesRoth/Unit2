2507a577f08dce58702375d81ceaf32aa55b54db
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-05 08:37:00 -0500

    Updated from Brython Server: 2/5/2018 8:36:59 AM

--- ageCalculator.py
+++ ageCalculator.py
@@ -1,1 +1,10 @@
+#James Roth
+#2/5/18
+#ageCalculator.py - age and current date
+
+from datetime import date
+
+year=int(input("Enter the year you were born: "))
+month=int(input("Enter the month you were born: "))
+day=int(input("Enter the day you were born: "))
 
