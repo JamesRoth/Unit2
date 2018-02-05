7c72a3856d9ca673933d71d240e3b4f637ff0c14
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-05 08:31:00 -0500

    Updated from Brython Server: 2/5/2018 8:30:59 AM

--- slope2.py
+++ slope2.py
@@ -7,8 +7,12 @@ x2=float(input("What is the second x coordinate? "))
 y1=float(input("What is the first y coordinate? "))
 y2=float(input("What is the second y coordinate? "))
 
-slope=float(round((y1-y2)/(x1-x2), 3))
-print("The slope is", slope)
+
+if (x1-x2)!=0:
+    slope=float(round((y1-y2)/(x1-x2), 3))
+    print("The slope is", slope)
+else:
+    print("The slope is 0")
 
 b=float(y1-slope*x1)
 print("The equation of the line is y =",slope,"x +", b)
