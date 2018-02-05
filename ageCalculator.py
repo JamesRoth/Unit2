dcfa1b21d0351984a3e350bbb28d1f13aa9050e0
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-05 08:46:46 -0500

    Updated from Brython Server: 2/5/2018 8:46:45 AM

--- ageCalculator.py
+++ ageCalculator.py
@@ -12,4 +12,7 @@ if date.today().month==month and date.today().day==day:
     print("Happy birthday!")
 if date.today().month<=month:
     if date.today().day<day:
-        print("You are", date.today().year-(year+1), "years old".)
+        print("You are", date.today().year-(year+1), "years old.")
+    else: 
+        print("You are", date.today().year-year, "years old.")
+    
