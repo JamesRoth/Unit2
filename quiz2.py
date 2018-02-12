c1115b5f49ac153574ae1c96dbc03bd29f2c58ea
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-12 08:24:38 -0500

    Updated from Brython Server: 2/12/2018 8:24:38 AM

--- quiz2.py
+++ quiz2.py
@@ -17,6 +17,6 @@ if "p" in word1:
 if "p" in word2:
     print("p is in", word2)
 if "p" in word1 and word2:
-    print("p is in" word1, "and", word2)
+    print("p is in", word1, "and", word2)
 else:
     print("p is not in", word1, "or", word2)
