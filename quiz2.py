c1143341deb0cf649bf61965dfbe1327b08ec1f8
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-12 08:18:47 -0500

    Updated from Brython Server: 2/12/2018 8:18:46 AM

--- quiz2.py
+++ quiz2.py
@@ -1,1 +1,11 @@
+#James Roth
+#2/12/18
+#quiz2.py - if statements quiz
 
+word1=input("Enter a word: ")
+word2=input("Enter a word: ")
+
+if len.(word1)>len.(word2):
+    print(word1, "is longer than", word2)
+else:
+    print(word1, "is shorter than", word2)
