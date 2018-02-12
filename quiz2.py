b6fa7f027d9d6618a92d917292a40de7f9703a62
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-12 08:31:56 -0500

    Updated from Brython Server: 2/12/2018 8:31:55 AM

--- quiz2.py
+++ quiz2.py
@@ -13,11 +13,14 @@ else:
     print(word1, "and", word2, "are the same length")
 
 if "p" in word1 or word2:
-    if "p" in word1:
-        print("p is in", word1)
-    if "p" in word2:
-        print("p is in", word2)
-    if "p" (in word1 and word2):
+        if "p" in word1 and word2:
         print("p is in", word1, "and", word2)
-elif "p" not (in word1 or word2):
+    
+    elif "p" in word1:
+        print("p is in", word1)
+        
+    elif "p" in word2:
+        print("p is in", word2)
+        
+elif "p" not in (word1 or word2):
     print("p is not in", word1, "or", word2)
