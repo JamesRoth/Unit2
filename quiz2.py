f9df3d07abca6b3cc540dad6d1bc2de2f7b81c2c
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-12 08:21:35 -0500

    Updated from Brython Server: 2/12/2018 8:21:34 AM

--- quiz2.py
+++ quiz2.py
@@ -11,4 +11,9 @@ elif len(word1)<len(word2):
     print(word1, "is shorter than", word2)
 else:
     print(word1, "and", word2, "are the same length")
-    
+
+if "p" in word1:
+    if "p" in word2:
+        print("p is in", word1, "and", word2)
+    else:
+        print("p is in", word1, "but not" word2)
